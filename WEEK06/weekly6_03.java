7f3217de276aa2888e9e1d9832476ed7c14a1438
lsvphanidhar31 <lsvphanidhar31@gmail.com>
2026-09-07 11:12:46 +0530

    Create weekly6_03.java

---
+++ WEEK06/weekly6_03.java
@@ -0,0 +1,15 @@
+public static String twoStrings(String s1, String s2) {
+    boolean[] seen = new boolean[26];
+
+    for (char c : s1.toCharArray()) {
+        seen[c - 'a'] = true;
+    }
+
+    for (char c : s2.toCharArray()) {
+        if (seen[c - 'a']) {
+            return "YES";
+        }
+    }
+
+    return "NO";
+}
