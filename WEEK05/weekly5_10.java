18c7699a04b3a037fc8b62db1419476f3f507623
lsvphanidhar31 <lsvphanidhar31@gmail.com>
2026-09-07 11:07:46 +0530

    Create weekly5_10.java

---
+++ WEEK05/weekly5_10.java
@@ -0,0 +1,20 @@
+class weekly5_10 {
+    public static void search(String pat, String txt) {
+        int n = txt.length();
+        int m = pat.length();
+
+        for (int i = 0; i <= n - m; i++) {
+            int j;
+
+            for (j = 0; j < m; j++) {
+                if (txt.charAt(i + j) != pat.charAt(j)) {
+                    break;
+                }
+            }
+
+            if (j == m) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
