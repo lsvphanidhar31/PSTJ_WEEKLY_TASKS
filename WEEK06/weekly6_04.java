faee32b3a665231c032d2c0ac1a17c7bf1d135cc
lsvphanidhar31 <lsvphanidhar31@gmail.com>
2026-09-07 11:16:39 +0530

    Create weekly6_04.java

---
+++ WEEK06/weekly6_04.java
@@ -0,0 +1,9 @@
+class weekly6_04 {
+    public boolean rotateString(String s, String goal) {
+        if (s.length() != goal.length()) {
+            return false;
+        }
+
+        return (s + s).contains(goal);
+    }
+}
